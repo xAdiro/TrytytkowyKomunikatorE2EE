ad46518fd421e6f6b3f59178023a3a556888f4c8
KaHet <102814829+KaHet@users.noreply.github.com>
2023-01-27 18:30:40 +0100

    Added Change Password Button

--- trytytkowy_komunikator/chat_app/urls.py
+++ trytytkowy_komunikator/chat_app/urls.py
@@ -14,5 +14,6 @@ urlpatterns = [
     path("accept-friend-request/", actions.accept_friend_request),
     path("", views.chat),
     path("change-password/", views.change_password_page),
+    path("change-password-action/", views.change_password_action),
     path("update-pub-key/", views.update_pub_key)
 ]
